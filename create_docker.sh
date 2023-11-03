11d18eaa280efcb59e006d02455f81503ed18dee
Pablo Bort <pablobort98@gmail.com>
2023-11-03 11:12:58 +0100

    Update create_docker.sh

--- create_docker.sh
+++ create_docker.sh
@@ -1,2 +1,2 @@
 sudo docker build -t myportfolio .
-sudo docker run -it -p 3000:3000 -p 8000:8000 --name myportfolio myportfolio
+sudo docker run -itd -p 3000:3000 -p 8000:8000 --name myportfolio myportfolio
